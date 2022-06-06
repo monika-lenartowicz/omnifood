a45e9711fef74d94215d0e5e70cdccca8dd97f32
Monika Lenartowicz <nika.nel@interia.pl>
2022-06-06 12:24:40 +0200

    added scrolling smooth efect to nav

--- script.js
+++ script.js
@@ -5,4 +5,30 @@ const changeClassToMenuMobile = () => {
 	header.classList.toggle('nav-open');
 };
 
+const yearElement = document.querySelector('.year');
+const currentYear = new Date().getFullYear();
+yearElement.textContent = currentYear;
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function (link) {
+	link.addEventListener('click', function (e) {
+		e.preventDefault();
+		const href = link.getAttribute('href');
+
+		if (href === '#')
+			window.scrollTo({
+				top: 0,
+				behavior: 'smooth',
+			});
+
+		if (href !== '#' && href.startsWith('#')) {
+			const sectionEl = document.querySelector(href);
+			sectionEl.scrollIntoView({ behavior: 'smooth' });
+		}
+
+		if (link.classList.contains('main-navigation-link')) header.classList.toggle('nav-open');
+	});
+});
+
 menu.addEventListener('click', changeClassToMenuMobile);
